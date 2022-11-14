5696eec27dd77bada73e2e9e5d8dc5fc726594cd
vonund <vonund@gmail.com>
2022-11-15 01:00:40 +0200

    feat: add promo block

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,6 +11,7 @@ module.exports = {
         'inner-500': `inset 0 0 0 1px ${colors.slate[500]}`,
         'inner-xs-300': `inset 0 0 0 2px ${colors.slate[300]}`,
         'inner-xs-500': `inset 0 0 0 2px ${colors.slate[500]}`,
+        'inner-xs-white': `inset 0 0 0 1px ${colors.white}`,
       }
     },
   },
